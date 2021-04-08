1774cc3fbeb39a130747d19323fe71cf728f05ab
denistita <denistita43@gmail.com>
2021-04-08 11:27:11 -0500

    First commit

---
+++ src/routes/crmRoutes.js
@@ -0,0 +1,26 @@
+import { addNewContact, getContacts, getContactWithID, updateContact, deleteContact } from "../controllers/crmController";
+
+const routes = (app) => {
+  app
+    .route("/contact")
+    .get(
+      (req, res, next) => {
+        //middleware
+        console.log(`Request from ${req.originalUrl}`);
+        console.log(`Request type:${req.method}`);
+        next();
+      },
+      getContacts
+    )
+    //post endpoint
+    .post(addNewContact);
+
+  app
+    .route("/contact/:contactID")
+    // get a specific contact
+    .get(getContactWithID) 
+    // updating a specific contact   
+    .put(updateContact)
+    .delete(deleteContact)
+};
+export default routes;
